e1e300d73eaaf214d30847e63b1d6716c286e1a9
Stefan Hüsges <Tronsha@gmx.de>
2014-11-27 10:07:27 +0100

    Update tooltip.php

--- tooltip.php
+++ tooltip.php
@@ -16,53 +16,51 @@
  * License URI:       https://raw.githubusercontent.com/tronsha/wp-tooltip-plugin/master/LICENSE
  */
 
-defined( 'ABSPATH' ) or ( @include_once explode( 'wp-content', __DIR__ )[0] . '/wp-hide.php' ) or die;
+defined('ABSPATH') or (@include_once explode('wp-content', __DIR__)[0] . '/wp-hide.php') or die;
 
 add_action(
     'init',
     function () {
-        if ( ! is_admin()) {
+        if (!is_admin()) {
             wp_register_style(
                 'tooltipster',
-                plugin_dir_url( __FILE__ ) . 'tooltipster/css/tooltipster.css',
+                plugin_dir_url(__FILE__) . 'tooltipster/css/tooltipster.css',
                 array(),
                 '3.3.0'
             );
             wp_register_script(
                 'tooltipster',
-                plugin_dir_url( __FILE__ ) . 'tooltipster/js/jquery.tooltipster.js',
-                array( 'jquery' ),
+                plugin_dir_url(__FILE__) . 'tooltipster/js/jquery.tooltipster.js',
+                array('jquery'),
                 '3.3.0'
             );
-            wp_enqueue_style( 'tooltipster' );
-            wp_enqueue_script( 'tooltipster' );
+            wp_enqueue_style('tooltipster');
+            wp_enqueue_script('tooltipster');
         }
     }
 );
 
 add_shortcode(
     'tooltip',
-    function ( $att = array(), $content = null ) {
-        $content  = do_shortcode( $content );
-        $tooltips = get_posts( array(
+    function ($att = array(), $content = null) {
+        $content  = do_shortcode($content);
+        $tooltips = get_posts(array(
             'offset'         => 0,
             'category_name'  => 'tooltips',
             'posts_per_page' => - 1,
             'orderby'        => 'ID',
             'order'          => 'ASC',
-        ) );
+        ));
         foreach ($tooltips as $tooltip) {
-            $content = str_replace( $tooltip->post_title,
-                '<span class="tooltip" title="' . $tooltip->post_content . '">' . $tooltip->post_title . '</span>',
-                $content );
+            $content = str_replace($tooltip->post_title, '<span class="tooltip" title="' . $tooltip->post_content . '">' . $tooltip->post_title . '</span>', $content);
         }
         $content .= '
-		<script>
-			jQuery(document).ready(function() {
-				jQuery(\'.tooltip\').tooltipster();
-			});
-		</script>
-		';
+        <script>
+            jQuery(document).ready(function() {
+                jQuery(\'.tooltip\').tooltipster();
+            });
+        </script>
+        ';
         return $content;
     }
 );
